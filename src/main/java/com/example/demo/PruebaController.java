843fb667860814f4aa9064980d1e4f9ca2214c4c
wilian B <buendia25andre@gmail.com>
2024-10-31 14:46:43 -0500

    configuracion inicial

---
+++ src/main/java/com/example/demo/PruebaController.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@RequestMapping("/saludar")
+public class PruebaController {
+    @GetMapping("/prueba")
+    public String saludar(){
+        return "Hola mundo";
+    }
+}
